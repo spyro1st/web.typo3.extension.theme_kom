19eaad3744a441ba987fc4aa69a1a69e8f7eece9
Kevin Ulrich Moschallski <kevin@digitalpatrioten.com>
2017-05-04 20:03:12 +0200

    Basic styling for start page, fonts etc.

--- ext_tables.php
+++ ext_tables.php
@@ -8,4 +8,8 @@ if (TYPO3_MODE === 'BE') {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
         'Configuration/TypoScript',
         'Kanditat-O-Mat Theme');
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/tsconfig.txt">'
+    );
 };
